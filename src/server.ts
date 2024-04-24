760179410a996901b95431a1d27074438ea1418d
MaskedRedstonerProZ <maskedredstonerproz@gmail.com>
2024-04-25 01:14:24 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,13 @@ import expressionRoutes from "./routes/expressions";
 const app = express();
 const port = 5000;
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
